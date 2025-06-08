855fecdf61e83bdb5ac0114583be8cb84ee38a84
habibsain <habibsain25@gmail.com>
2025-06-08 20:49:39 +0530

    replaced try-except with helper error function

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from .models import *
 
@@ -11,10 +11,7 @@ def index(request):
                     })
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk = question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist");
+    question = get_object_or_404(Question, pk=question_id)
 
     choices = question.choice_set.all()
     return render(request, "polls/detail.html", 
