769900b0bb0dc8fcbc2bd9ad7f65134356edb910
habibsain <habibsain25@gmail.com>
2025-06-09 21:07:03 +0530

    added url for vote

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -7,6 +7,7 @@ app_name = "polls"
 urlpatterns = [
     path("", views.index, name = "index"),
 
-    #check for invalid question id later
-    path("<int:question_id>/", views.detail, name="detail"),
+    
+    path("<int:question_id>/", views.detail, name = "detail"),
+    path("<int:question_id>/vote/", views.vote, name = "vote"),
 ]
