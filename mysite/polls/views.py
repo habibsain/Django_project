afaaccb1bc050f0418a0af8cb701a3693339ebac
habibsain <habibsain25@gmail.com>
2025-06-11 18:57:00 +0530

    updated view

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,8 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import Http404, HttpResponseRedirect
 from django.db.models import F
 from django.urls import reverse
-
-from .models import *
+from .models import Question, Choice
 
 # Create your views here.
 def index(request):
