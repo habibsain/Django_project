67e50e54239c02c5037732ec35700cf99242d33b
habibsain <habibsain25@gmail.com>
2025-06-22 23:36:20 +0530

    updated ulrs to make it generic

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -5,10 +5,10 @@ from . import views
 app_name = "polls"
 
 urlpatterns = [
-    path("", views.index, name = "index"),
-    path("<int:question_id>/", views.detail, name = "detail"),
-    path("<int:question_id>/vote/", views.vote, name = "vote"),
-    path("<int:question_id>/result/", views.result, name = "result"),
+    path("", views.IndexView.as_view(), name = "index"),
+    path("<int:pk>/", views.DetailView.as_view(), name = "detail"),
+    path("<int:pk>/vote/", views.vote, name = "vote"),
+    path("<int:question_id>/result/", views.ResultView.as_view(), name = "result"),
     path("new/", views.add_page, name = "add"),
     path("add/", views.add_question, name = "add_question"),
     path("<int:question_id>/choice_page", views.choice_page, name = "choice_page"),
