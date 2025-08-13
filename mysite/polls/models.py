408eb1b97f97cd33ebafc3dd06d778bddede459c
habibsain <habibsain25@gmail.com>
2025-08-13 20:35:21 +0530

    edited queston model and added test

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,14 +1,19 @@
 from django.db import models
+from django.utils import timezone
+import datetime
 
 
 # Create your models here.
 
 class Question(models.Model):
     question_text = models.TextField(max_length=100, unique=True)
-    pub_date = models.DateField("Date published")
+    pub_date = models.DateTimeField("Date published")
     
     def __str__(self):
         return self.question_text
+    
+    def was_published_recently(self):
+        return (self.pub_date >= timezone.now() - datetime.timedelta(days=1)) and (self.pub_date <= timezone.now())
 
     
 
--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from django.utils import timezone
+import datetime 
 
-# Create your tests here.
+from .models import Question
+
+class QuestionModelTests(TestCase):
+    def test_was_published_recentlly(self):
+        time = timezone.now() + datetime.timedelta(days=30) # pub date is 30 days in future
+        future_question = Question(pub_date=time)
+        self.assertIs(future_question.was_published_recently(), False)
